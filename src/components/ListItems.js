be855f5459a9a47ac51f79765b4795b3a2289a64
akavishka-ifs <akavishka@workwave.com>
2022-10-01 22:18:13 +0530

    fixed minor nit picks in side bar menu items

--- src/components/ListItems.js
+++ src/components/ListItems.js
@@ -1,52 +1,51 @@
-import * as React from 'react';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import ListSubheader from '@mui/material/ListSubheader';
-import DashboardIcon from '@mui/icons-material/Dashboard';
-import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
-import PeopleIcon from '@mui/icons-material/People';
-import BarChartIcon from '@mui/icons-material/BarChart';
-import LayersIcon from '@mui/icons-material/Layers';
-import AssignmentIcon from '@mui/icons-material/Assignment';
-import { Link } from 'react-router-dom';
+import * as React from "react";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import ListSubheader from "@mui/material/ListSubheader";
+import DashboardIcon from "@mui/icons-material/Dashboard";
+import PeopleIcon from "@mui/icons-material/People";
+import PaidIcon from '@mui/icons-material/Paid';
+import DocumentScannerIcon from '@mui/icons-material/DocumentScanner';
+import AssignmentIcon from "@mui/icons-material/Assignment";
+import { Link } from "react-router-dom";
 
 export const mainListItems = (
   <React.Fragment>
     <ListSubheader component="div" inset>
       Budget Management
     </ListSubheader>
-    <Link to='/lookups'> 
+    <Link to="/lookups">
       <ListItemButton>
-          <ListItemIcon>
-              <DashboardIcon />
-          </ListItemIcon>
+        <ListItemIcon>
+          <DashboardIcon />
+        </ListItemIcon>
         <ListItemText primary="LookUps" />
       </ListItemButton>
     </Link>
-    <Link to='/vendors'>
-    <ListItemButton>
-      <ListItemIcon>
-        <PeopleIcon />
-      </ListItemIcon>
-      <ListItemText primary="Vendors" />
-    </ListItemButton>
+    <Link to="/vendors">
+      <ListItemButton>
+        <ListItemIcon>
+          <PeopleIcon />
+        </ListItemIcon>
+        <ListItemText primary="Vendors" />
+      </ListItemButton>
     </Link>
-    <Link to='/documents'>
-    <ListItemButton>
-      <ListItemIcon>
-        <BarChartIcon />
-      </ListItemIcon>
-      <ListItemText primary="Documents" />
-    </ListItemButton>
+    <Link to="/documents">
+      <ListItemButton>
+        <ListItemIcon>
+          <DocumentScannerIcon />
+        </ListItemIcon>
+        <ListItemText primary="Documents" />
+      </ListItemButton>
     </Link>
-    <Link to='/transactions'>
-    <ListItemButton>
-      <ListItemIcon>
-        <LayersIcon />
-      </ListItemIcon>
-      <ListItemText primary="Transactions" />
-    </ListItemButton>
+    <Link to="/transactions">
+      <ListItemButton>
+        <ListItemIcon>
+          <PaidIcon />
+        </ListItemIcon>
+        <ListItemText primary="Transactions" />
+      </ListItemButton>
     </Link>
   </React.Fragment>
 );
