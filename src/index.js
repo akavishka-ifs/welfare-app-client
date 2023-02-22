2a4a29d622555b656161963a336a8d6adaf17888
akavishka-ifs <akavishka@workwave.com>
2023-02-22 09:50:02 +0530

    added devtools support for react-query

--- src/index.js
+++ src/index.js
@@ -1,20 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import AppContextProvider from './contexts/AppContext';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import AppContextProvider from "./contexts/AppContext";
+import { QueryClientProvider, QueryClient } from "react-query";
+import {ReactQueryDevtools} from 'react-query/devtools';
+
+const queryClient = new QueryClient();
 
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter>{/*this component let's us use all the react router features in our app, also this is what mapps the url address with the navigation*/}
-    <AppContextProvider>
-      <App />
-    </AppContextProvider>
+    <BrowserRouter>
+      {/*this component let's us use all the react router features in our app, also this is what mapps the url address with the navigation*/}
+      <QueryClientProvider client={queryClient}>
+        <AppContextProvider>
+          <App />
+        </AppContextProvider>
+        <ReactQueryDevtools initialIsOpen={false} position='bottom-right'/>
+      </QueryClientProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
